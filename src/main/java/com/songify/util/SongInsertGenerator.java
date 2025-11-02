d2679736b7f927b79d7d5f9b37263f3bad798e9c
Anastazja Glowska <anastazjaglowska12345@gmail.com>
2025-11-02 09:46:46 +0100

    New unit tests written to SongifyCrudFacade

---
+++ src/main/java/com/songify/util/SongInsertGenerator.java
@@ -0,0 +1,4 @@
+package com.songify.util;
+
+class SongInsertGenerator {
+}
---
+++ src/main/resources/db/migration/V16__added_optimistic_locking_field_version.sql
@@ -0,0 +1,26 @@
+ALTER TABLE album
+    ADD version BIGINT;
+
+ALTER TABLE artist
+    ADD version BIGINT;
+
+ALTER TABLE genre
+    ADD version BIGINT;
+
+ALTER TABLE song
+    ADD version BIGINT;
+
+ALTER TABLE genre
+    ALTER COLUMN name DROP NOT NULL;
+
+ALTER TABLE album
+    ALTER COLUMN uuid DROP NOT NULL;
+
+ALTER TABLE artist
+    ALTER COLUMN uuid DROP NOT NULL;
+
+ALTER TABLE genre
+    ALTER COLUMN uuid DROP NOT NULL;
+
+ALTER TABLE song
+    ALTER COLUMN uuid DROP NOT NULL;
